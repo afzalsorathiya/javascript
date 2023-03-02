ff60c7f152d47fd785e3b6cbbcd7eec957c6c383
Afzal Sorathiya <88094538+afzalsorathiya@users.noreply.github.com>
2023-03-02 06:59:54 +0000

    add variable file

--- 01_Introduction/01_variable.js
+++ 01_Introduction/01_variable.js
@@ -1,5 +1,6 @@
 // Cannot change const value
 const accountID = 123456
+// console.log(accountID);
 let accountEmail = "afzal@gmail.com"
 var accountPass = "1234"
 accountCity = "Vadodara"
@@ -9,4 +10,4 @@ accountPass = "4321"
 accountCity = "surat"
 console.log(accountID);
 // print multiple values
-console.table([accountEmail,accountPass,accountCity])
+console.table([accountID,accountEmail,accountPass,accountCity])
