49bfa156cfb6e23bf8c6558bd5c85eb1f06d7478
Afzal Sorathiya <88094538+afzalsorathiya@users.noreply.github.com>
2023-03-03 10:36:38 +0000

    05 string file added

---
+++ 01_Introduction/05_string.js
@@ -0,0 +1,17 @@
+const name = "hitesh";
+const repoCount = 50;
+
+//This is not recommended practice
+// console.log(name + repoCount + " Value");
+
+//best practice
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// JS string operations
+const gameName = new String("hitesh-hc-com");
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+
+// console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
